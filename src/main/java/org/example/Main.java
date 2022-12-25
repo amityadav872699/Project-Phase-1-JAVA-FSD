63462e164213e78c4647d0a6ac5ea4a5c72fb684
Amit Yadav (amitya2) <amitya2@cisco.com>
2022-12-25 12:30:25 +0530

    First commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import Functioanlity.Menu;
+
+public class Main {
+        public static final String path = "C:\\Users\\amitya2\\Downloads\\Lockedme";
+
+        public static void main(String[] args) {
+            Menu menu = new Menu();
+            menu.introScreen();
+            menu.mainMenu();
+        }
+
+}
